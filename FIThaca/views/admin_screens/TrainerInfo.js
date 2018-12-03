9b603130064262dcaf4fd62d58f0a3dfdcfc5c7a
rking5 <44852080+rking5@users.noreply.github.com>
2018-12-03 13:10:32 -0500

    Ryan stuff (#6)
    * Update AddTrainer.js
    
    * Update AdminHome.js
    
    * Update Reports.js
    
    * Update TrainerInfo.js
    
    * Update TrainerList.js

--- FIThaca/views/admin_screens/TrainerInfo.js
+++ FIThaca/views/admin_screens/TrainerInfo.js
@@ -8,10 +8,53 @@ export default class TrainerInfoScreen extends React.Component {
         title: 'Trainer Info',
     };
 
+    constructor(props) {
+        super(props);
+
+        const name = this.props.navigation.getParam('name', 'NO-NAME');
+       
+        //query database for actual trainer information
+        this.state = {
+            name: name,
+            type: 'Enter Trainer Name',
+            contact: 'Enter Contact Information',
+            current_package: ' the package identifier',
+            past_packages: [
+                {key: '1', id: 'past_package_identifier'}, 
+                {key: '2', id: 'past_package_identifier_2'}
+            ]
+        };
+
+        const willFocusSubscription = this.props.navigation.addListener(
+            'willFocus',
+            this._updatePackages 
+        );
+    }
+
+    _updatePackages = () => {
+        //get packages from database
+    }
+
+    _renderItem = data => {
+        return (
+            <View>
+                <TouchableHighlight onPress={() => this.props.navigation.navigate('PackageInfo', {id: data.item.id})} underlayColor="blue">
+		            <Text style={styles.row}>{data.item.id}</Text>
+		        </TouchableHighlight> 
+            </View>
+        );
+    };
+
     render() {
         return (
-            <View style={styles.container}> 
-                <Text>Trainer Info</Text>
+            <View style={styles.container}>  
+                <Text>{this.state.name}{'\n'}{this.state.type}{'\n'}{this.state.contact}</Text>
+                <Text>Current Package: {'\n'}</Text>
+                <TouchableHighlight onPress={() => this.props.navigation.navigate('PackageInfo', {id: this.state.current_package})} underlayColor="blue">
+                    <Text>{this.state.current_package}</Text>
+                </TouchableHighlight>
+                <FlatList data={this.state.past_packages} renderItem={this._renderItem}/>
+                <Button title='Add Package' onPress={()=>this.props.navigation.navigate('AddPackage', {trainer: this.state.name})}/>
             </View>
         );
     }
--- FIThaca/views/admin_screens/TrainerList.js
+++ FIThaca/views/admin_screens/TrainerList.js
@@ -8,10 +8,46 @@ export default class TrainerListScreen extends React.Component {
         title: 'Trainers',
     };
 
+    constructor(props) {
+        super(props);
+
+        //get list from database
+        this.state = {trainers: [
+            {key: '1', name: 'Fake Name'},
+            {key: '2', name: 'Joe From Somewhere'},
+            {key: '3', name: 'Bob'},
+            {key: '4', name: 'Evil Person'},
+            {key: '5', name: 'No One'},
+            {key: '6', name: 'Bob'},
+            {key: '7', name: 'Evil Person'},
+            {key: '8', name: 'No One'} 
+        ]};
+
+        const willFocusSubscription = this.props.navigation.addListener(
+            'willFocus',
+            this._updateList 
+        );
+    }
+
+    _updateList = () => {
+        //fetch data from database
+    }
+
+    _renderItem = data => {
+        return (
+            <View>
+                <TouchableHighlight onPress={()=>this.props.navigation.navigate('TraingerInfo', {name: data.item.name})} underlayColor="blue">
+		            <Text style={styles.row}>{data.item.name}</Text>
+		        </TouchableHighlight> 
+            </View>
+        );
+    };
+
     render() {
         return (
             <View style={styles.container}> 
-                <Text>Trainer List</Text>
+                <FlatList data={this.state.clients} renderItem={this._renderItem}/>
+                <Button title='Add Trainer' onPress={()=>this.props.navigation.navigate('AddTrainer')}/>
             </View>
         );
     }
