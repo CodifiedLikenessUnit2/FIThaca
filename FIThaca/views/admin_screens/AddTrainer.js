3bead2daaab112aaa6b5572cf03529e2f7444aa6
rking5 <44852080+rking5@users.noreply.github.com>
2018-12-03 12:33:19 -0500

    Update AddTrainer.js

--- FIThaca/views/admin_screens/AddTrainer.js
+++ FIThaca/views/admin_screens/AddTrainer.js
@@ -8,10 +8,34 @@ export default class AddTrainerScreen extends React.Component {
         title: 'Add Trainer',
     };
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            first: '',
+            last: '',
+            email: '',
+            phone: ''
+        }
+    }
+
+    _addTrainer = () => {
+        //add trainer to database
+        Alert.alert("trainer added");
+        this.props.navigation.navigate('Trainers');
+    }
+
     render() {
         return (
             <View style={styles.container}>  
-                <Text>Add Trainer</Text>
+                <TextInput onChangeText={(first) => this.setState({first})} placeholder={'First Name'}/>
+                <TextInput onChangeText={(last) => this.setState({last})} placeholder={'Last Name'}/>
+                <TextInput onChangeText={(email) => this.setState({email})} placeholder={'Email Address'}/>
+                <TextInput onChangeText={(phone) => this.setState({phone})} placeholder={'Phone Number'}/>
+
+
+                <Button title='Add Trainer' onPress={this._addTrainer}/>
+                <Button title='Cancel' onPress={()=>this.props.navigation.navigate('Trainers')}/>
             </View>
         );
     }
