2fafb3cc4f92c1ea62c1ad1b015a8d23388242fa
paxmeny <36705016+paxmeny@users.noreply.github.com>
2018-12-06 17:26:40 -0500

    Update PastSessions.js

--- FIThaca/views/trainer_screens/PastSessions.js
+++ FIThaca/views/trainer_screens/PastSessions.js
@@ -7,11 +7,20 @@ export default class PastSessionsScreen extends React.Component {
     static navigationOptions = {
         title: 'Past Sessions',
     };
+constructor(props){
+    super(props);
+    this.state = {key};
+  }
 
     render() {
         return (
             <View style={styles.container}>  
                 <Text>Past Sessions</Text>
+            <Button
+				title="Go Back"
+				onPress={() => this.props.navigation.goBack()}
+					/>
+				<Text>data</Text>
             </View>
         );
     }
