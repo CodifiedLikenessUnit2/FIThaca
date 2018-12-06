e384da883a0f494fa8ac0cbc3b8862f6312ff8d3
paxmeny <36705016+paxmeny@users.noreply.github.com>
2018-12-06 17:27:48 -0500

    Update TrainerClientList.js

--- FIThaca/views/trainer_screens/TrainerClientList.js
+++ FIThaca/views/trainer_screens/TrainerClientList.js
@@ -7,11 +7,22 @@ export default class TrainerClientsScreen extends React.Component {
     static navigationOptions = {
         title: 'Clients',
     };
-
+constructor(props){
+    super(props);
+    this.state = {key};
+  }
     render() {
         return (
             <View style={styles.container}>  
                 <Text>Trainer Clients</Text>
+                <Text>this.state.data</Text>
+				<Button
+				title="Go Back"
+				onPress={() => this.props.navigation.goBack()}/>
+				<Button
+          title="Go to Client Session Screen"
+          onPress={() => this.props.navigation.navigate(PastClientSessionsScreen:{key:""})}
+        />
             </View>
         );
     }
