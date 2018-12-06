917f738576141c93bc4556044ee4e566834dfc5e
paxmeny <36705016+paxmeny@users.noreply.github.com>
2018-12-06 17:28:44 -0500

    Update UpcomingSessions.js

--- FIThaca/views/trainer_screens/UpcomingSessions.js
+++ FIThaca/views/trainer_screens/UpcomingSessions.js
@@ -7,11 +7,20 @@ export default class UpcomingSessionsScreen extends React.Component {
     static navigationOptions = {
         title: 'Sessions',
     };
+constructor(props){
+    super(props);
+    this.state = {key};
+  }
 
     render() {
         return (
             <View style={styles.container}>  
                 <Text>Upcoming Sessions</Text>
+                <Text>{this.state.data}</Text>
+            <Button
+				title="Go Back"
+				onPress={() => this.props.navigation.goBack()}
+					/>
             </View>
         );
     }
