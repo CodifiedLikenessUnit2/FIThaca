6156101cdb506c3bb77333acd8a86d359e661b5f
rking5 <44852080+rking5@users.noreply.github.com>
2018-12-19 23:25:06 -0500

    Update ClientInfoTrainer.js

--- FIThaca/views/trainer_screens/ClientInfoTrainer.js
+++ FIThaca/views/trainer_screens/ClientInfoTrainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, TouchableHighlight, FlatList } from 'react-native';
+import { View, Text, Button, TouchableHighlight, FlatList, Alert } from 'react-native';
 import styles from '../../styles/styles';
 
 export default class ClientInfoScreenTrainer extends React.Component {
@@ -14,46 +14,49 @@ export default class ClientInfoScreenTrainer extends React.Component {
 
         //query database for actual client information
         this.state = {
-            name: name,
-            type: 'client_type',
-            contact: 'contact_info',
-            current_package: 'package_identifier',
-            sessions: [
-                {key: '1', id: 'session_identifier'},
-                {key: '2', id: 'session_identifier_2'}
-            ]
+            renderCheck:false,
+            isLoading: true,
         };
+    }
+    
+    componentDidMount(){
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+    
+        const url = 'http://cs-ithaca.eastus.cloudapp.azure.com/~mogrady/fithaca/trainerGetClient.php'
+        var data = {clientID: 2};
+    
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: myHeaders
+            })
+            .then((response) => response.json())
+            .then(responseJson => {
+              this.setState({
+                dataSource: responseJson,
+              })
+            })
+            .catch(error => Alert.alert('Error:'+ error));
+        }
 
-const willFocusSubscription = this.props.navigation.addListener(
-    'willFocus',
-    this._updatePackages
-);
-}
-
-_updatePackages = () => {
-//get packages from database
-}
-
-_renderItem = data => {
-return (
-    <View>
-        <TouchableHighlight onPress={() => this.props.navigation.navigate('PackageInfo', {id: data.item.id})} underlayColor="blue">
-            <Text style={styles.row}>{data.item.id}</Text>
-        </TouchableHighlight>
-    </View>
-);
-};
-
-render() {
-return (
-    <View style={styles.container}>
-        <Text>{this.state.name}</Text>
-        <Text>{this.state.type}</Text>
-        <Text>{this.state.contact}</Text>
-        <Text>Number of Sessions Remaining: n/a</Text>
-        <Text>Upcoming Sessions:</Text>
-        <FlatList data={this.state.sessions} renderItem={this._renderItem}/>
-        <Button title='Past Sessions' onPress={() => this.props.navigation.navigate('ClientPastSessions', {client: this.state.name})}/>
+        _renderItem = data => {
+            if(this.state.renderCheck==false){
+            return (
+                <View>
+                    <TouchableHighlight onPress={()=>this.props.navigation.navigate('ClientInfoT', {name: data.item.name})} underlayColor="blue">
+                        <Text>{data.item.clientID} {data.item.clientName} {'\n'}{data.item.clientType} {'\n'}{data.item.time}</Text>
+                    </TouchableHighlight>
+                </View>
+            );
+            }
+            this.state.renderCheck=true;
+        };
+        
+        render() {
+            return (
+                <View style={styles.container}>
+                    <FlatList data={this.state.dataSource} renderItem={this._renderItem} keyExtractor={({time}, index) => time}/>
     </View>
 );
 }
