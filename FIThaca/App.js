c8bf0d2152fe277854db0c22845e6187de8e76d2
rking5 <44852080+rking5@users.noreply.github.com>
2018-12-20 19:43:47 -0500

    Update App.js

--- FIThaca/App.js
+++ FIThaca/App.js
@@ -2,6 +2,5 @@ import adminNav from './views/Admin';
 import trainerNav from './views/Trainer';
 import onboardNav from './views/Onboarding';
 
-//export default adminNav;
-//export default trainerNav;
+//All this does is make sure that the app will start at the login screen
 export default onboardNav;
